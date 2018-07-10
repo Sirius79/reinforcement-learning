184517187a1913ef628cb1590948a4697c286f92
Rajorshi Chaudhuri <rajorshi.chaudhuri@spit.ac.in>
2018-07-10 18:56:47 +0530

    Create on_policy_MC_control.py

---
+++ monte-carlo/on_policy_MC_control.py
@@ -0,0 +1,83 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+from collections import defaultdict
+
+class Agent():
+
+    def __init__(self, epsilon):
+
+        self.epsilon = epsilon
+        self.policy = defaultdict(lambda: np.random.random(size=2))
+        self.Q = defaultdict(lambda: np.random.randint(26,size=2))
+        self.returns_hit = defaultdict(list)
+        self.returns_stick = defaultdict(list) 
+
+    def generate(self):
+        '''
+            Generate an episode following the policy
+        '''
+        state = env.reset()
+        states = []
+        actions = []
+        rewards = []
+
+        states.append(state)
+        
+        for step in itertools.count():
+            
+            action = np.argmax(self.policy[state])
+            actions.append(action)
+            state,reward,done,_ = env.step(action)
+            states.append(state)
+            rewards.append(reward)
+
+            if done:
+                break
+
+        states.pop()
+        
+        return states, actions, rewards
+            
+
+    def play(self):
+        
+        for episode in range(episode_num):
+
+            states, actions, rewards = self.generate()
+            G = 0.0
+            
+            for step in range(len(states)):
+
+                
+                G += rewards[step]
+
+                if states[step] not in states[0:step] and actions[step] not in actions[0:step]:
+                    action_taken = actions[step]
+                    if action_taken == 0:
+                        current_state = self.returns_hit[states[step]]
+                        current_state.append(G)
+                        self.Q[states[step][action_taken]] = sum(current_state)/len(current_state)
+                    else:
+                        current_state = self.returns_stick[states[step]]
+                        current_state.append(G)
+                        self.Q[states[step][action_taken]] = sum(current_state)/len(current_state)
+
+                    a_star = np.argmax(self.Q[states[step]])
+                    
+                    for a in range(2):
+
+                        if a == a_star:
+                            self.policy[states[step]][a] = 1 - self.epsilon + (self.epsilon / 2)
+                        else:
+                            self.policy[states[step]][a] = self.epsilon / 2
+
+        return self.policy
+        
+episode_num = 1000
+
+
+env = gym.make('Blackjack-v0')
+agent = Agent(0.5)
+v_pi = agent.play()
